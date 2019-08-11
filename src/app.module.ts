45d32aa9c6aacd17507bf763ff9ef662f85392e2
Osvaldo Fernandez <osvaldofernandez@KAIS-MAC43.local>
2019-08-11 15:50:51 +0200

    added connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://ofarinas12:123@cluster0-qg49r.mongodb.net/test?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class  AppModule {}
